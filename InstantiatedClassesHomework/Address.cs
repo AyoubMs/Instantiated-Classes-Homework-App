93c18ba31067fe7503fa6ae2101cc0b7d6e8dd8a
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:06:51 +0200

    add app

---
+++ InstantiatedClassesHomework/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantiatedClassesHomework
+{
+    public class Address
+    {
+        public int houseNumber { get; set; }
+        public int streetNumber { get; set; }
+        public int zipCode { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+    }
+}
